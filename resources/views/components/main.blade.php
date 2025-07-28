5d25712c50fee4576936fa8b4cdbc9fd87e80ba6
Luca Scaruffi <lucascaruffi@MacBook-Pro-di-Luca.local>
2025-07-28 15:41:39 +0200

    aggiunta file

--- resources/views/components/main.blade.php
+++ resources/views/components/main.blade.php
@@ -7,9 +7,7 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>{{ $title ?? 'Il Mio Portfolio' }}</title>
 
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 
     <link rel="stylesheet" href="{{ asset('css/style.css') }}">
     {{ $head_extra ?? '' }}
@@ -27,9 +25,7 @@
 
     <x-footer />
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous">
-    </script>
+
     <script src="{{ asset('js/script.js') }}"></script>
     {{ $scripts_extra ?? '' }}
 
